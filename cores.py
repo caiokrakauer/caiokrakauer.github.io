1d6b31e6acc21165dde501bd2b7fb10e6df692d0
caiokrakauer <caiokrakauer@gmail.com>
2021-05-19 16:28:54 -0300

    commit 2 pastas

--- cores.py
+++
@@ -1,8 +0,0 @@
-import random
-cores = ["amarelo","azul","branco","laranja","preto","rosa","roxo","verde","vermelho"]
-cores2 = ["amarelo","azul","branco","laranja","preto","rosa","roxo","verde","vermelho"]
-random.shuffle(cores)
-random.shuffle(cores2)
-cores.extend(cores2)
-for i in cores:
-    print(i)
